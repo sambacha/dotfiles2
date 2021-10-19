48d615cbc67402ff217636a7cab9ac786776b2fd
sam bacha <sam@manifoldfinance.com>
2021-10-18 19:39:45 -0700

    terminal.rc

---
+++ terminalrc.sh
@@ -0,0 +1,8 @@
+# vi: ft=bash
+# $SHELL.rc options
+shopt -s dirspell globstar histappend histverify nocaseglob nullglob
+
+for config in "$XDG_CONFIG_HOME"/bashrc.d/*.bash{,.local}; do
+  # shellcheck disable=SC1090
+  test -r "$config" && . "$config"
+done
